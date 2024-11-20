9b171df735db93d0e13a9125c3170212928025d3
rishabh2310113 <rishabh2310113@gmail.com>
2024-11-20 14:15:34 +0530

    added a responsive layout screen

---
+++ utils/responsive_layout.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobile;
+  final Widget tablet;
+  final Widget desktop;
+
+  const ResponsiveLayout({
+    required this.mobile,
+    required this.tablet,
+    required this.desktop,
+  });
+
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width < 600;
+
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 600 &&
+      MediaQuery.of(context).size.width < 1024;
+
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 1024;
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    if (screenWidth >= 1024) {
+      return desktop;
+    } else if (screenWidth >= 600) {
+      return tablet;
+    } else {
+      return mobile;
+    }
+  }
+}
